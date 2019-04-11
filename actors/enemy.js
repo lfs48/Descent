eb16ed4ad2e40e6a1569c5c7c0e2d86f6a731c0e
Lucas <lucasfschraier@gmail.com>
2019-04-11 18:00:34 -0400

    Initial commit

---
+++ actors/enemy.js
@@ -0,0 +1,17 @@
+class Enemy extends CircleActor {
+    handleCollision(otherActor) {
+        const { xBoundUp, xBoundDown, yBoundUp, yBoundDown } = this.collisionBox();
+        const otherCollision = otherActor.collisionBox();
+        const otherXBoundUp = otherCollision.xBoundUp;
+        const otherXBoundDown = otherCollision.xBoundDown;
+        const otherYBoundUp = otherCollision.yBoundUp;
+        const otherYBoundDown = otherCollision.yBoundDown;
+
+        if (typeof otherActor === 'Bullet') {
+            debugger
+            if (yBoundDown <= otherYBoundUp && yBoundUp > otherYBoundUp) {
+                this.remove = true;
+            }
+        }
+    }
+}

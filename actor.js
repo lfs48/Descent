a5c868f56e928c30fd8df2a118b576b5a85b8bf7
Lucas <lucasfschraier@gmail.com>
2019-04-11 14:36:15 -0400

    Edit constructor to take in width and height instead of radius

--- actor.js
+++ actor.js
@@ -1,23 +1,24 @@
 class Actor {
-    constructor(x, y, vx, vy, r, drawFunction) {
+    constructor(x, y, vx, vy, width, height, drawFunction) {
         this.x = x;
         this.y = y;
         this.vx = vx;
         this.vy = vy;
-        this.r = r;
+        this.width = width;
+        this.height = height;
         this.drawFunction = drawFunction.bind(this);
     }
 
     willCollide(otherActor) {
-        const xBoundUp = this.x + this.vx + this.r;
-        const xBoundDown = this.x + this.vx - this.r;
-        const yBoundUp = this.y + this.vy + this.r;
-        const yBoundDown = this.y + this.vy - this.r
+        const xBoundUp = this.x + this.vx + this.width;
+        const xBoundDown = this.x + this.vx - this.width;
+        const yBoundUp = this.y + this.vy + this.height;
+        const yBoundDown = this.y + this.vy - this.height;
 
-        const otherXBoundUp = otherActor.x + otherActor.vx + otherActor.r;
-        const otherXBoundDown = otherActor.x + otherActor.vx - otherActor.r;
-        const otherYBoundUp = otherActor.y + otherActor.vy + otherActor.r;
-        const otherYBoundDown = otherActor.y + otherActor.vy - otherActor.r;
+        const otherXBoundUp = otherActor.x + otherActor.vx + otherActor.width;
+        const otherXBoundDown = otherActor.x + otherActor.vx - otherActor.width;
+        const otherYBoundUp = otherActor.y + otherActor.vy + otherActor.height;
+        const otherYBoundDown = otherActor.y + otherActor.vy - otherActor.height;
 
         const overlapX = !(
             (xBoundUp >= otherXBoundUp && xBoundDown >= otherXBoundUp) ||

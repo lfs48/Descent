b77ebeba8a7c78bc0e5bbfb24b7e788433bbc143
Lucas <lucasfschraier@gmail.com>
2019-04-11 16:14:44 -0400

    Handle collisions by setting x veloctity away from colliding side

--- actors/player.js
+++ actors/player.js
@@ -1,9 +1,17 @@
-class Player extends Actor {
-    drawFunction(ctx) {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.width, 0, Math.PI*2, false);
-        ctx.fillStyle = "white";
-        ctx.fill();
-        ctx.closePath();
+class Player extends CircleActor {
+
+    handleCollision(otherActor) {
+        const { xBoundUp, xBoundDown, yBoundUp, yBoundDown } = this.collisionBox();
+        const otherCollision = otherActor.collisionBox();
+        const otherXBoundUp = otherCollision.xBoundUp;
+        const otherXBoundDown = otherCollision.xBoundDown;
+        const otherYBoundUp = otherCollision.yBoundUp;
+        const otherYBoundDown = otherCollision.yBoundDown;
+
+        if (xBoundUp >= otherXBoundDown && xBoundDown < otherXBoundDown) {
+            this.vx = -10;
+        } else if (xBoundDown <= otherXBoundUp && xBoundUp > otherXBoundUp) {
+            this.vx = 10;
+        }
     }
 }

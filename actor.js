11cbbf4c4e51643e0fdec98e9c407a4d94eeec7e
Lucas <lucasfschraier@gmail.com>
2019-04-11 14:17:23 -0400

    Add missing const tags to variables

--- actor.js
+++ actor.js
@@ -1,5 +1,5 @@
 class Actor {
-    constructor(x, y, vx, vy, r) {
+    constructor(x, y, vx, vy, r, drawCb) {
         this.x = x;
         this.y = y;
         this.vx = vx;
@@ -13,10 +13,10 @@ class Actor {
         const yBoundUp = this.y + this.vy + this.r;
         const yBoundDown = this.y + this.vy - this.r
 
-        otherXBoundUp = otherActor.x + otherActor.vx + otherActor.r;
-        otherXBoundDown = otherActor.x + otherActor.vx - otherActor.r;
-        otherYBoundUp = otherActor.y + otherActor.vy + otherActor.r;
-        otherYBoundDown = otherActor.y + otherActor.vy - otherActor.r;
+        const otherXBoundUp = otherActor.x + otherActor.vx + otherActor.r;
+        const otherXBoundDown = otherActor.x + otherActor.vx - otherActor.r;
+        const otherYBoundUp = otherActor.y + otherActor.vy + otherActor.r;
+        const otherYBoundDown = otherActor.y + otherActor.vy - otherActor.r;
 
         const overlapX = !(
             (xBoundUp >= otherXBoundUp && xBoundDown >= otherXBoundUp) ||

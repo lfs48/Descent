67b0c19ecdadadc32886ab8aa11142ab040e37e7
Lucas <lucasfschraier@gmail.com>
2019-04-11 11:26:33 -0400

    Define methods for changing actor's horizontal speed

--- actor.js
+++ actor.js
@@ -7,8 +7,20 @@ class Actor {
         this.r = r;
     }
 
-    move() {
+    updatePos() {
         this.x += this.vx;
         this.y += this.vy;
+    }
+
+    moveRight() {
+        this.vx = 2;
+    }
+
+    moveLeft() {
+        this.vx = -2;
+    }
+
+    stopHorzMove() {
+        this.vx = 0;
     }
 }

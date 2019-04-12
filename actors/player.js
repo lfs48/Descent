19d905bf2012441178a13205d34a07a10fc8e27e
Lucas <lucasfschraier@gmail.com>
2019-04-12 11:48:10 -0400

    Update handleCollision to check class of otherActor, handle cases for Wall and Obstacle

--- actors/player.js
+++ actors/player.js
@@ -29,12 +29,17 @@ class Player extends CircleActor {
         const otherYBoundUp = otherCollision.yBoundUp;
         const otherYBoundDown = otherCollision.yBoundDown;
 
-        if (xBoundUp >= otherXBoundDown && xBoundDown < otherXBoundDown) {
-            this.vx = -10;
-        } else if (xBoundDown <= otherXBoundUp && xBoundUp > otherXBoundUp) {
-            this.vx = 10;
-        } else if (yBoundUp >= otherYBoundDown && yBoundDown < otherYBoundDown) {
-            this.grounded = true;
+        if (otherActor instanceof Wall) {
+            if (xBoundUp >= otherXBoundDown && xBoundDown < otherXBoundDown) {
+                this.vx = -10;
+            } else if (xBoundDown <= otherXBoundUp && xBoundUp > otherXBoundUp) {
+                this.vx = 10;
+            }
+        }
+        if (otherActor instanceof Obstacle) {
+            if (yBoundUp >= otherYBoundDown && yBoundDown < otherYBoundDown) {
+                this.grounded = true;
+            }
         }
     }
 }

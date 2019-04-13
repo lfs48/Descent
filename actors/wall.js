80487e6d9a423f0e4d457a6d492e118e20a78044
lucas schraier <lucasfschraier@gmail.com>
2019-04-13 17:00:34 -0400

    Update dimensions and drawFunction method to use sprite

--- actors/wall.js
+++ actors/wall.js
@@ -2,15 +2,38 @@ class Wall extends RectActor {
 
     constructor(x, y, vx, vy) {
         super(x, y, vx, vy);
-        this.width = 30;
-        this.height = 720;
+        this.width = 26;
+        this.height = 107;
+        this.activeSprite = new Sprite(this, 'assets/wall.png', 1);
+    }
+
+    randomColor() {
+        const r = Math.random();
+        if (r > 0.9) {
+            return "lightblue";
+        } else if (r > 0.8) {
+            return "red";
+        } else if (r > 0.7) {
+            return "blue";
+        } else if (r > 0.6) {
+            return "purple";
+        } else if (r > 0.5) {
+            return "white";
+        } else if (r > 0.4) {
+            return "green";
+        } else if (r > 0.3) {
+            return "pink";
+        } else if (r > 0.2) {
+            return "orange";
+        } else if (r > 0.1) {
+            return "yellow";
+        } else {
+            return "brown";
+        }
     }
 
     drawFunction(ctx) {
-        ctx.beginPath();
-        ctx.rect(this.x, this.y, this.width, this.height);
-        ctx.fillStyle = "white";
-        ctx.fill();
-        ctx.closePath();
+        this.activeSprite.draw(ctx);
+        this.activeSprite.update();
     }
 }

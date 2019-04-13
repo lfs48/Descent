ecb6b238e3013ec887a13af8692da4eb42d197d3
lucas schraier <lucasfschraier@gmail.com>
2019-04-13 13:31:58 -0400

    Remove reverse spritesheet, as the reversed sprite will have its own class

--- sprites/playerStandingSprite.js
+++ sprites/playerStandingSprite.js
@@ -1,10 +1,10 @@
-class PlayerFallingSprite extends Sprite {
+class PlayerStandingSprite extends Sprite {
     constructor(actor) {
         super(actor);
         this.ticksPerFrame = 60;
         this.numFrames = 1;
         this.spritesheet = new Image();
-        this.spritesheet.src = "assets/player-falling.gif";
+        this.spritesheet.src = "assets/player-standing.gif";
     }
     
 }

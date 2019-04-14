c4bf48a9682daa589532bdd06f3985f55f6ef67a
lucas schraier <lucasfschraier@gmail.com>
2019-04-14 15:17:07 -0400

    Refactor to be superclass for various enemy types

--- actors/enemy.js
+++ actors/enemy.js
@@ -1,18 +1,4 @@
 class Enemy extends Actor {
-    constructor(x, y, vx, vy) {
-        super(x, y, vx, vy);
-        this.width = 80;
-        this.height = 70;
-        this.randomPath = false;
-        this.generateRandomPath = this.generateRandomPath.bind(this);
-
-        this.direction = vx > 0 ? 'Right' : 'Left';
-        this.sprites = {
-            defaultRight: new Sprite(this, 'assets/big-ghost-right.png', 6),
-            defaultLeft: new Sprite(this, 'assets/big-ghost-left.png', 6)
-        };
-        this.activeSprite = this.sprites[`default${this.direction}`];
-    }
 
     handleCollision(otherActor) {
         const { xBoundUp, xBoundDown, yBoundUp, yBoundDown } = this.collisionBox();
@@ -39,13 +25,6 @@ class Enemy extends Actor {
                 this.updateSprite();
             }
         }
-    }
-
-    generateRandomPath() {
-        this.randomPath = false;
-        this.vx = Math.random() > 0.5 ? 3 : -3;
-        this.direction = this.vx > 0 ? 'Right' : 'Left';
-        this.updateSprite();
     }
 
     updatePos() {

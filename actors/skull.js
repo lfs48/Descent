c34af2e3fc9a18f2ca4ac7480104352ff079446e
lucas schraier <lucasfschraier@gmail.com>
2019-04-14 15:16:47 -0400

    Change superclass to Enemy

--- actors/skull.js
+++ actors/skull.js
@@ -1,4 +1,4 @@
-class Skull extends Actor {
+class Skull extends Enemy {
     constructor(x, y, vx, vy) {
         super(x, y, vx, vy);
         this.width = 40;
@@ -12,42 +12,6 @@ class Skull extends Actor {
             defaultLeft: new Sprite(this, 'assets/skull-left.png', 4)
         };
         this.activeSprite = this.sprites[`default${this.direction}`];
-    }
-
-    handleCollision(otherActor) {
-        const { xBoundUp, xBoundDown, yBoundUp, yBoundDown } = this.collisionBox();
-        const otherCollision = otherActor.collisionBox();
-        const otherXBoundUp = otherCollision.xBoundUp;
-        const otherXBoundDown = otherCollision.xBoundDown;
-        const otherYBoundUp = otherCollision.yBoundUp;
-        const otherYBoundDown = otherCollision.yBoundDown;
-
-        if (otherActor instanceof Bullet) {
-            if (yBoundDown <= otherYBoundUp && yBoundUp > otherYBoundUp) {
-                this.remove = true;
-            }
-        }
-
-        if (otherActor instanceof Wall) {
-            if (xBoundUp >= otherXBoundDown && xBoundDown < otherXBoundDown) {
-                this.vx = -3;
-                this.direction = 'Left';
-                this.updateSprite();
-            } else if (xBoundDown <= otherXBoundUp && xBoundUp > otherXBoundUp) {
-                this.vx = 3;
-                this.direction = 'Right';
-                this.updateSprite();
-            }
-        }
-    }
-
-    updatePos() {
-        if (!this.randomPath) {
-            this.randomPath = true;
-            setTimeout(this.generateRandomPath, 500);
-        }
-        this.x += this.getVx();
-        this.y += this.getVy();
     }
 
     generateRandomPath() {

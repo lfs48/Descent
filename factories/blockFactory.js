56c0d07bddd22ac544b13c019c20987f3960c53d
Lucas <lucasfschraier@gmail.com>
2019-04-15 11:52:07 -0400

    Fix typos

--- factories/blockFactory.js
+++ factories/blockFactory.js
@@ -7,8 +7,6 @@ class blockFactory {
             [ [60,750], [90,750], [120,750], [150,750], [180, 750] ],
             [ [90,750], [120,750], [150,750], [180, 750], [210, 750] ]
         ];
-        this.randomArrangement = this.randomArrangement.bind(this);
-        this.generateBlocks = this.generateBlocks.bind(this);
     }
 
     randomArrangement() {
@@ -19,12 +17,15 @@ class blockFactory {
     generateBlocks() {
         const arrangement = this.randomArrangement();
         const blocks = [];
+        const blockGroup = new BlockGroup();
         for(let i = 0; i < arrangement.length; i++) {
             const x = arrangement[i][0];
             const y = arrangement[i][1];
-            const block = new Block({x: x, y: y, vy: this.screen.getGravity});
+            const block = new Block({x: x, y: y, vy: this.screen.getGravity, blockGroup: blockGroup});
             blocks.push(block);
+            blockGroup.addBlock(block);
         };
+        blockGroup.initializeCollision();
         return blocks;
     }
 

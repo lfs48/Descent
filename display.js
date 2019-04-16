7f6004b8d1ea6368f8cfb3d6b7b7bd8379aae075
Lucas <lucasfschraier@gmail.com>
2019-04-16 10:20:55 -0400

    Add keydown event listener that starts game when user presses enter on splash screen

--- display.js
+++ display.js
@@ -1,23 +1,41 @@
 class Display {
     constructor() {
+        const splashCanvas = document.getElementById("descent-canvas");
         const descentCanvas = document.getElementById("descent-canvas");
         const scoreCanvas = document.getElementById("score-canvas");
         const healthbarCanvas = document.getElementById("healthbar-canvas");
+
+        this.splash = new Splash(splashCanvas);
         this.screen = new Screen(descentCanvas);
         this.score = new Score(scoreCanvas, this.screen);
         this.healthbar = new Healthbar(healthbarCanvas, this.screen);
-
-        document.addEventListener("keydown", this.screen.keyDownHandler, false);
-        document.addEventListener("keyup", this.screen.keyUpHandler, false);
-
-        this.gameStarted = true;
+        this.gameStarted = false;
         this.draw = this.draw.bind(this);
+        this.keyDownHandler = this.keyDownHandler.bind(this);
+        document.addEventListener("keydown", this.keyDownHandler, false);
+        // document.addEventListener("keydown", this.screen.keyDownHandler, false);
+        // document.addEventListener("keyup", this.screen.keyUpHandler, false);
+
+        
     }
 
     draw() {
-        this.screen.draw();
-        this.score.draw();
-        this.healthbar.draw();
+        if (this.gameStarted) {
+            this.screen.draw();
+            this.score.draw();
+            this.healthbar.draw();
+        } else {
+            this.splash.draw();
+        }
+    }
+
+    keyDownHandler(e) {
+        if(e.key == "Enter") {
+            this.gameStarted = true;
+            document.removeEventListener("keydown", this.keyDownHandler);
+            document.addEventListener("keydown", this.screen.keyDownHandler, false);
+            document.addEventListener("keyup", this.screen.keyUpHandler, false);
+        }
     }
     
 }

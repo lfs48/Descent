908a85e374da295de030c35e523d8fcf201d1e54
Lucas <lucasfschraier@gmail.com>
2019-04-16 10:20:35 -0400

    Add keydown event listener that starts game when user presses enter on splash screen

--- splash.js
+++ splash.js
@@ -6,10 +6,11 @@ class Splash {
         this.ctx.font = "50px Arial";
 
         this.draw = this.draw.bind(this);
-        this.clear = this.clear.bind(this);
+        // this.clear = this.clear.bind(this);
     }
 
     draw() {
+        this.ctx.fillStyle="white";
         this.ctx.fillText(`Play`, 12, 25);
         this.ctx.fillText(`Instructions`, 12, 50);
     }

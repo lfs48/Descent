fb750c9e98a43fc44a78a0bd09ce16bc7a579dd5
Lucas <lucasfschraier@gmail.com>
2019-04-16 10:36:25 -0400

    Render cursor

--- splash.js
+++ splash.js
@@ -6,6 +6,8 @@ class Splash {
         this.ctx = canvas.getContext("2d");
         this.ctx.font = "50px Arial";
 
+        this.cursor = new Visual({x:50, y:50, width:80, height:66, file:'assets/arrow-right.png', numFrames:2, framesPerTick:45});
+
         this.draw = this.draw.bind(this);
         this.clear = this.clear.bind(this);
         this.keyDownHandler = this.keyDownHandler.bind(this);
@@ -14,9 +16,10 @@ class Splash {
     draw() {
         this.clear();
         this.ctx.fillStyle="white";
-        this.ctx.fillText(`Play`, 100, 100);
-        this.ctx.fillText(`Instructions`, 100, 200);
-        this.ctx.fillText(`About Me`, 100, 300);
+        this.ctx.fillText(`Play`, 150, 100);
+        this.ctx.fillText(`Instructions`, 150, 200);
+        this.ctx.fillText(`About Me`, 150, 300);
+        this.cursor.drawFunction(this.ctx);
     }
 
     clear() {

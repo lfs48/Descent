efe43dd12eeb88446faa19dfa585745d2c7d3d5b
lucas schraier <lucasfschraier@gmail.com>
2019-04-21 16:50:09 -0400

    Edit pattern keys to be functionals rather than static values so that a random value is properly generated each call

--- factories/enemyFactory.js
+++ factories/enemyFactory.js
@@ -5,12 +5,12 @@ class enemyFactory {
         this.enemyTypes = [Ghost, Skull, Bouncy];
         this.generateEnemies = this.generateEnemies.bind(this);
         this.patterns = [
-            [ {x: 140 + (Math.random() * 100) , y: 740} ],
-            [ {x: 260 + (Math.random() * 100) , y: 740} ],
-            [ {x: 140, y: 740}, {x: 360, y: 820} ],
-            [ {x: 140 + (Math.random() * 40), y: 740}, {x: 180 + (Math.random() * 40), y: 820} ],
-            [ {x: 260 + (Math.random() * 40), y: 740}, {x: 300 + (Math.random() * 40), y: 820} ],
-            [ {x: 140 + (Math.random() * 60), y: 740}, {x: 220 + (Math.random() * 60), y: 820}, {x: 300 + (Math.random() * 60), y: 900} ]
+            [ {x: () => 140 + (Math.random() * 100) , y: () => 740} ],
+            [ {x: () => 260 + (Math.random() * 100) , y: () => 740} ],
+            [ {x: () => 140, y: () => 740}, {x: () => 350, y: () => 820} ],
+            [ {x: () => 140 + (Math.random() * 40), y: () => 740}, {x: () => 180 + (Math.random() * 40), y: () => 820} ],
+            [ {x: () => 260 + (Math.random() * 40), y: () => 740}, {x: () => 300 + (Math.random() * 40), y: () => 820} ],
+            [ {x: () => 140 + (Math.random() * 60), y: () => 740}, {x: () => 220 + (Math.random() * 60), y: () => 820}, {x: () => 290 + (Math.random() * 60), y: () => 900} ]
         ];
     }
 
@@ -30,7 +30,7 @@ class enemyFactory {
         for(let i = 0; i < pattern.length; i++) {
             const {x, y} = pattern[i];
             const enemyType = this.getRandomEnemyType();
-            const enemy = new enemyType({x:x, y:y, vy:this.screen.getGravity, screen:this.screen});
+            const enemy = new enemyType({x:x(), y:y(), vy:this.screen.getGravity, screen:this.screen});
             enemies.push(enemy);
         }
         return enemies;

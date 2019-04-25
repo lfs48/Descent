f2c909e28a89221ebb937bac7c674a8f127bd815
Lucas <lucasfschraier@gmail.com>
2019-04-25 11:22:40 -0400

    Edit sprite to get image from url rather than assets folder

--- actors/obstacle.js
+++ actors/obstacle.js
@@ -4,7 +4,7 @@ class Obstacle extends Actor {
         super(options);
         this.height = 38;
         this.width = 152;
-        this.activeSprite = new Sprite(this, './assets/obstacle.png');
+        this.activeSprite = new Sprite(this, 'https://66.media.tumblr.com/84c6d92701f8f3bed914a40f3ba228cf/tumblr_pqivzbEAOM1wejsx8o1_250.png');
     }
 
 }

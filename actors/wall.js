bb5822aeb96553fe7863b9d5afad8e5740e36cd4
Lucas <lucasfschraier@gmail.com>
2019-04-25 11:17:30 -0400

    Edit sprite to get image from url rather than assets folder

--- actors/wall.js
+++ actors/wall.js
@@ -4,7 +4,7 @@ class Wall extends Actor {
         super(options);
         this.width = 26;
         this.height = 107;
-        this.activeSprite = new Sprite(this, 'assets/wall.png');
+        this.activeSprite = new Sprite(this, 'https://66.media.tumblr.com/327f7d0a97af719228c2851d6bd4763b/tumblr_pqivcvGggD1wejsx8o1_100.png');
     }
 
 }
